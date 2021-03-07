3bfb9f21437d847f6ddf260a9178c6f533fab792
kevinmcvey <mcveymk@gmail.com>
2021-03-07 13:58:00 -0800

    Add a 1s buffer between channel switch and audio
    This resolves an issue that causes the first sound effect after
    switching channels to play very quietly (or not at all).

--- src/audio-commands.js
+++ src/audio-commands.js
@@ -6,6 +6,7 @@ const TEXT_TO_VOICE_CHANNEL = {
   'bandit-jam': 'Bandit Jam',
   'untitled-new-show': 'Untitled New Show',
   'bot-development': 'Bot Development',
+  'bot-development-2': 'Bot Development 2',
   'the-ocean': 'The Ocean'
 }
 
@@ -22,13 +23,28 @@ const SOUNDS = {
   }
 }
 
+// Discord.jd VoiceChannel.join() returns a Promise that doesn't resolve until its child
+// VoiceConnection emits a `ready` event. That ready event seems a bit early, though? If a sound
+// is played *immediately* after ready is emitted, the sound may be played at a low volume or
+// simply not at all. A brief buffer period seems to resolve the issue, though it's a bit inelegant.
+const SWITCH_CHANNEL_REST_PERIOD_MS = 1000;
+
 let activeVoiceConnection;
 
 async function connectToVoiceChannel(bot, channelName) {
-  return bot.channels
+  const voiceConnection = await bot.channels
           .cache.array()
           .filter(channel => channel.type === 'voice' && channel.name === channelName)[0]
           .join();
+
+  await sleep(SWITCH_CHANNEL_REST_PERIOD_MS);
+  return voiceConnection;
+}
+
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
 async function logError(error) {
@@ -70,6 +86,7 @@ exports.handleMessage = async function(bot, message) {
   }
 
   const voiceChannel = TEXT_TO_VOICE_CHANNEL[message.channel.name];
+
   if (!activeVoiceConnection || voiceChannel != activeVoiceConnection.channel.name) {
     activeVoiceConnection = await connectToVoiceChannel(bot, voiceChannel);
   }
